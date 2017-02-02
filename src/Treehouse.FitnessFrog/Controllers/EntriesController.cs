29b3215dafbbbb1b6d88a32896522e5c63add05f
mhereford <mhereford@gmail.com>
2017-02-01 22:53:41 -0500

    Updated EntriesController Add Method to handle invalid values.

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -52,12 +52,12 @@ namespace Treehouse.FitnessFrog.Controllers
                                 bool? exclude,
                                 string notes)
         {
-            ViewBag.Date = date;
-            ViewBag.ActivityID = activityId;
-            ViewBag.Duration = duration;
-            ViewBag.Intensity = intensity;
-            ViewBag.Exclude = exclude;
-            ViewBag.Notes = notes;
+            ViewBag.Date = ModelState["Date"].Value.AttemptedValue;
+            ViewBag.ActivityID = ModelState["ActivityID"].Value.AttemptedValue;
+            ViewBag.Duration = ModelState["Duration"].Value.AttemptedValue;
+            ViewBag.Intensity = ModelState["Intensity"].Value.AttemptedValue;
+            ViewBag.Exclude = ModelState["Exclude"].Value.AttemptedValue;
+            ViewBag.Notes = ModelState["Notes"].Value.AttemptedValue;
 
             return View();
         }
